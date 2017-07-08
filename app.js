1b397e7576bbf540675b64637509cee2f6cf75aa
gulajava <gulajava.mini@gmail.com>
2017-07-08 15:46:28 +0700

    Inisialisasi awal Git

---
+++ app.js
@@ -0,0 +1,45 @@
+// logika game pukul guling
+var vm = new Vue({
+    el: '#vue-game',
+    data: {
+        kesehatanNumber: 100,
+        isEnded: false
+    },
+    methods: {
+        pukulGuling: function () {
+            
+            var randomNumber = this.randomValPukul();
+            this.kesehatanNumber = this.kesehatanNumber - randomNumber;
+
+            if(this.kesehatanNumber <= 0) {
+                this.isEnded = true;
+            };
+        },
+        restartGame: function () {
+            
+            this.kesehatanNumber = 100;
+            this.isEnded = false;
+        },
+        randomValPukul: function() {
+
+            var randomNumber =  Math.floor(Math.random() * 10);
+            return randomNumber;
+        }
+    },
+    computed: {
+        healthBarComputed : function() {
+
+            var styleWidthBar = {
+                width: this.kesehatanNumber + "%"
+            }
+            return styleWidthBar;
+        },
+        bagStatusComputed : function () {
+            
+            var imageBagStatus = {
+                burst : this.isEnded
+            }
+            return imageBagStatus;
+        }
+    }
+});
