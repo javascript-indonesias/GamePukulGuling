da86167c6dfb601d90405a8e6f76b3017eec69eb
gulajava <gulajava.mini@gmail.com>
2017-07-10 14:31:33 +0700

    Reformat kode

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ var vm = new Vue({
     },
     methods: {
         pukulGuling: function () {
-            
+
             var randomNumber = this.randomValPukul();
             this.kesehatanNumber = this.kesehatanNumber - randomNumber;
 
-            if(this.kesehatanNumber <= 0) {
+            if (this.kesehatanNumber <= 0) {
                 this.isEnded = true;
             };
         },
         restartGame: function () {
-            
+
             this.kesehatanNumber = 100;
             this.isEnded = false;
         },
-        randomValPukul: function() {
+        randomValPukul: function () {
 
-            var randomNumber =  Math.floor(Math.random() * 10);
+            var randomNumber = Math.floor(Math.random() * 10);
             return randomNumber;
         }
     },
     computed: {
-        healthBarComputed : function() {
+        healthBarComputed: function () {
 
             var styleWidthBar = {
                 width: this.kesehatanNumber + "%"
             }
             return styleWidthBar;
         },
-        bagStatusComputed : function () {
-            
+        bagStatusComputed: function () {
+
             var imageBagStatus = {
-                burst : this.isEnded
+                burst: this.isEnded
             }
             return imageBagStatus;
         }
